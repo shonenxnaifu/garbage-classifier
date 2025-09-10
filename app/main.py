aa3503ea94aa2fc8a48c60a340169a9ec99d79b6
Pawitra Warda <pawitrawarda@gmail.com>
2025-09-10 10:26:06 +0700

    change api route

--- app/main.py
+++ app/main.py
@@ -97,73 +97,73 @@ def read_root():
     return {"message": "Garbage CLassifier API - Go to /api/docs for UI"}
 
 
+# @app.post(
+#     "/classify_bycats",
+#     response_model=ClassificationResult,
+#     tags=["Classification"],
+#     summary="Classify garbage item",
+#     description="Classify an image of garbage predefined categories",
+#     responses={
+#         200: {
+#             "description": "Successful classification",
+#             "content": {
+#                 "application/json": {
+#                     "example": {
+#                         "top_class": "electronic waste",
+#                         "confidence": 0.33171185851097107,
+#                         "predictions": {
+#                             "plastic bottle": 0.24136486649513245,
+#                             "glass bottle": 0.0022902777418494225,
+#                             "paper": 0.0015287415590137243,
+#                             "cardboard": 0.0012100355233997107,
+#                             "metal can": 0.24120794236660004,
+#                             "organic waste": 0.18023031949996948,
+#                             "electronic waste": 0.33171185851097107,
+#                             "textile": 0.00045594872790388763,
+#                         },
+#                     }
+#                 }
+#             },
+#         },
+#         400: {"description": "Invalid input"},
+#         500: {"description": "Internal server error"},
+#     },
+# )
+# async def classify_bycats(
+#     file: UploadFile = File(..., description="Image of garbage item"),
+#     categories: str = Query(
+#         default=",".join(DEFAULT_CLASSES),
+#         description="Comma-separated classification categories",
+#     ),
+# ):
+#     try:
+#         # Process inputs
+#         candidate_labels = [c.strip() for c in categories.split(",")]
+#
+#         image = load_image(await file.read())
+#
+#         # Run classification
+#         results = classify_image(image, candidate_labels)
+#
+#         # Find top prediction
+#         top_class = max(results, key=results.get)
+#
+#         return JSONResponse(
+#             status_code=200,
+#             content={
+#                 "top_class": top_class,
+#                 "confidence": results[top_class],
+#                 "predictions": results,
+#             },
+#         )
+#
+#     except Exception as e:
+#         logging.error(f"Classification error: {str(e)}")
+#         return {"error": "Classification failed"}
+
+
 @app.post(
-    "/classify",
-    response_model=ClassificationResult,
-    tags=["Classification"],
-    summary="Classify garbage item",
-    description="Classify an image of garbage predefined categories",
-    responses={
-        200: {
-            "description": "Successful classification",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "top_class": "electronic waste",
-                        "confidence": 0.33171185851097107,
-                        "predictions": {
-                            "plastic bottle": 0.24136486649513245,
-                            "glass bottle": 0.0022902777418494225,
-                            "paper": 0.0015287415590137243,
-                            "cardboard": 0.0012100355233997107,
-                            "metal can": 0.24120794236660004,
-                            "organic waste": 0.18023031949996948,
-                            "electronic waste": 0.33171185851097107,
-                            "textile": 0.00045594872790388763,
-                        },
-                    }
-                }
-            },
-        },
-        400: {"description": "Invalid input"},
-        500: {"description": "Internal server error"},
-    },
-)
-async def classify(
-    file: UploadFile = File(..., description="Image of garbage item"),
-    categories: str = Query(
-        default=",".join(DEFAULT_CLASSES),
-        description="Comma-separated classification categories",
-    ),
-):
-    try:
-        # Process inputs
-        candidate_labels = [c.strip() for c in categories.split(",")]
-
-        image = load_image(await file.read())
-
-        # Run classification
-        results = classify_image(image, candidate_labels)
-
-        # Find top prediction
-        top_class = max(results, key=results.get)
-
-        return JSONResponse(
-            status_code=200,
-            content={
-                "top_class": top_class,
-                "confidence": results[top_class],
-                "predictions": results,
-            },
-        )
-
-    except Exception as e:
-        logging.error(f"Classification error: {str(e)}")
-        return {"error": "Classification failed"}
-
-
-@app.post(
-    "/classify_simple",
+    "/classify_predef_cat",
     response_model=ClassificationResult,
     tags=["Classification"],
     summary="Classify garbage item with predefined categories",
@@ -194,7 +194,7 @@ async def classify(
         500: {"description": "Internal server error"},
     },
 )
-async def classify_img(
+async def classify_predef_cat(
     file: UploadFile = File(..., description="Image of garbage item"),
 ):
     try:
